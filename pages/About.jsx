c98081cff37a5d8a1756e5934da3160e08d144a4
Razi1212 <Farooqui.razi8700@gmail.com>
2025-01-23 18:47:33 +0530

    added experience ,about section with updated crousals

--- pages/About.jsx
+++ pages/About.jsx
@@ -20,30 +20,3 @@ const About = () => {
 };
 
 export default About;
-
-{/* <div className="flex flex-wrap">
-            <span className="box bg-zinc-800 p-3 mr-3 mb-3 rounded-lg text-gray-400 text-center inline-block transition-transform transform-gpu hover:scale-110 hover:text-white hover:bg-sky-700">
-              HTML
-            </span>
-            <span className="box bg-zinc-800 p-3 mr-3 mb-3 rounded-lg text-gray-400 text-center inline-block transition-transform transform-gpu hover:scale-110  hover:text-white hover:bg-sky-700">
-              CSS
-            </span>
-            <span className="box bg-zinc-800 p-3 mr-3 mb-3 rounded-lg text-gray-400 text-center inline-block transition-transform transform-gpu hover:scale-110 hover:text-white hover:bg-sky-700">
-              JavaScript
-            </span>
-            <span className="box bg-zinc-800 p-3 mr-3 mb-3 rounded-lg text-gray-400 text-center inline-block transition-transform transform-gpu hover:scale-110 hover:text-white hover:bg-sky-700">
-              Tailwind CSS
-            </span>
-            <span className="box bg-zinc-800 p-3 mr-3 mb-3 rounded-lg text-gray-400 text-center inline-block transition-transform transform-gpu hover:scale-110 hover:text-white  hover:bg-sky-700">
-              ReactJS
-            </span>
-            <span className="box bg-zinc-800 p-3 mr-3 mb-3 rounded-lg text-gray-400 text-center inline-block transition-transform transform-gpu hover:scale-110 hover:text-white  hover:bg-sky-700">
-              Next.js
-            </span>
-            <span className="box bg-zinc-800 p-3 mr-3 mb-3 rounded-lg text-gray-400 text-center inline-block transition-transform transform-gpu hover:scale-110 hover:text-white  hover:bg-sky-700" >
-              Redux
-            </span>
-            <span className="box bg-zinc-800 p-3 mr-3 mb-3 rounded-lg text-gray-400 text-center inline-block transition-transform transform-gpu hover:scale-110 hover:text-white  hover:bg-sky-700">
-              SQL
-            </span>
-          </div> */}
